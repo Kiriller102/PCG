8505a1722036cf04780ddd7c5ccda2a4923d925a
Kirill <94110888+Kiriller102@users.noreply.github.com>
2023-01-02 03:40:45 +0300

    add 3rd lab

--- Lab_4exe/Code/main.py
+++
@@ -1,9 +0,0 @@
-from MainWindow import MainWindow
-from PyQt5.QtWidgets import QApplication
-import sys
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mw = MainWindow()
-    mw.show()
-    sys.exit(app.exec_())
